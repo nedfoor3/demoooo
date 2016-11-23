67e1ec9398d9551444c7e149377df78f6d4eaa99
nedfoor3 <nedfoor@gmail.com>
2016-11-23 16:28:46 -0600

    Initial commit

---
+++ app/src/main/java/lfa/titanes/proyectofootball/com/demoooo/MainActivity.java
@@ -0,0 +1,13 @@
+package lfa.titanes.proyectofootball.com.demoooo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
